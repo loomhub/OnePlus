0122823c20eaa073b84455359d508073a439541a
loomhub <vjnyustern@gmail.com>
2024-04-17 18:54:15 -0400

    Fix property_master primary key

--- oneplus/mylibrary/models/property_master_model.py
+++ oneplus/mylibrary/models/property_master_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, Numeric,CheckConstraint
+from sqlalchemy import Column, Integer, String, Date, Numeric,CheckConstraint, UniqueConstraint
 from ..database.db import Base
 from sqlalchemy.orm import relationship
 
@@ -6,10 +6,14 @@ class PropertyMaster(Base):
     __tablename__ = "property_master"
 
     id = Column(Integer, primary_key=True, index=True)
-    property_name = Column(String,primary_key=True, index=True)
+    property_name = Column(String,unique=True, index=True, nullable=False)
     property_description = Column(String)
     llc = Column(String) 
     note = Column(String,CheckConstraint('note IN ("LLC","VJ","NG","PG")'))
     account_type = Column(String,CheckConstraint('account_type IN ("Checking","Savings")'))
     bank_name = Column(String)
     account_number = Column(Numeric(10, 0))
+    __table_args__ = (
+        UniqueConstraint('property_name', name='uix_property_name'),
+    )
+    
