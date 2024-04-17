7ee7cb27c50d83798763c08b6a4a7715fc630f7f
loomhub <vjnyustern@gmail.com>
2024-04-17 18:45:36 -0400

    Fix llc_master unique key

--- oneplus/mylibrary/models/llc_model.py
+++ oneplus/mylibrary/models/llc_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, Numeric,CheckConstraint
+from sqlalchemy import Column, Integer, String, Date, Numeric,CheckConstraint, UniqueConstraint
 from ..database.db import Base
 from sqlalchemy.orm import relationship
 
@@ -6,7 +6,10 @@ class LLCMaster(Base):
     __tablename__ = "llc_master"
 
     id = Column(Integer, primary_key=True, index=True)
-    llc = Column(String,primary_key=True, index=True)
+    llc = Column(String, index=True, nullable=False,unique = True)
     ein = Column(String)
     llc_address = Column(String) 
+    __table_args__ = (
+        UniqueConstraint('llc', name='uix_llc'),
+    )
     
