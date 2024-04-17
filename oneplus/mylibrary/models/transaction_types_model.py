e902402792bf102551ef7f1db41806b6f883b503
loomhub <vjnyustern@gmail.com>
2024-04-17 18:35:52 -0400

    Add transaction types

---
+++ oneplus/mylibrary/models/transaction_types_model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Date, Numeric,CheckConstraint,UniqueConstraint
+from ..database.db import Base
+from sqlalchemy.orm import relationship
+class LLCMaster(Base):
+    __tablename__ = "transaction_types"
+
+    id = Column(Integer, primary_key=True, index=True)
+    transaction_group = Column(String, index=True)
+    transaction_type = Column(String, index=True)
+    transaction_description = Column(String)
+    # Define the composite unique constraint
+    __table_args__ = (
+        UniqueConstraint('transaction_type', 'transaction_group', 
+                         name='uix_transaction_type_group'),
+    )
