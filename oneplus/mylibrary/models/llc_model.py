41f14db71445a464052d0b720009b0379a8fb6f0
loomhub <vjnyustern@gmail.com>
2024-04-20 09:12:46 -0400

    added description and formation date to llcs table

--- oneplus/mylibrary/models/llc_model.py
+++ oneplus/mylibrary/models/llc_model.py
@@ -10,6 +10,8 @@ class Llcs(Base):
     llc = Column(String, index=True, nullable=False,unique = True)
     ein = Column(String)
     llc_address = Column(String) 
+    llc_description = Column(String)  # New column for LLC description
+    formation_date = Column(Date)  # New column for formation date
     __table_args__ = (
         UniqueConstraint('llc', name='uix_llc'),
     )
