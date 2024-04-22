d319b59b82ef34d92d323f3c8d25241a9f293bf1
loomhub <vjnyustern@gmail.com>
2024-04-22 08:10:00 -0400

    Cleaned up LLC endpoint names

--- oneplus/mylibrary/controllers/llc_controller.py
+++ oneplus/mylibrary/controllers/llc_controller.py
@@ -88,7 +88,7 @@ async def create_or_update_llc(llcs_data: llcsListDTO, db: AsyncSession = Depend
             raise HTTPException(status_code=400, detail=str(e))
     return results
 ############################################################################################################
-@router.post("/upload_llcs_old", summary="Upload and save LLCs data from a CSV file")
+@router.post("/llcsxl", summary="Upload and save LLCs data from a CSV file")
 async def upload_and_save_llc_file(
     file: UploadFile = File(...),
     db: AsyncSession = Depends(get_session)
@@ -109,56 +109,6 @@ async def upload_and_save_llc_file(
             raise HTTPException(status_code=400, detail=str(e))
     return results
     
-
-############################################################################################################
-@router.post("/upload_llcs", summary="Upload, save, and load LLCs data from a CSV file")
-async def upload_save_and_load_llc_file(file: UploadFile = File(...)):
-    # Define the path where you want to save the file
-    file_location = f"uploads/{file.filename}"
-
-    # Check if the uploads directory exists, if not, create it
-    if not os.path.exists('uploads'):
-        os.makedirs('uploads')
-
-    # Write the contents of the uploaded file to a new file in the server
-    try:
-        with open(file_location, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Could not write to file: {str(e)}")
-
-    # Read the file into a DataFrame
-    try:
-        df = pd.read_csv(file_location)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to load file into DataFrame: {str(e)}")
-
-    return JSONResponse(status_code=200, content={"message": "File processed successfully", "data": df.to_dict(orient='records')})
-    
-############################################################################################################
-@router.post(
-        "/llcsxl",
-        summary="Create or edit multiple LLCs using excel file",
-        description="Create or edit multiple LLCs using excel file",
-        tags=["Upsert"],
-        )
-async def create_or_update_llc_excel(
-    file: UploadFile = File(...), 
-    db: AsyncSession = Depends(get_session)):
-
-    llc_filehandler = llcFileHandler(file)
-    llcs_data = llc_filehandler.extract_data_from_file()
-    llc_repository = LLCRepository(db)
-    llc_service = llcService(llc_repository)
-    results = []
-    for llc in llcs_data.llcs:
-        try:
-            created, result = await llc_service.create_or_update_llc(llc)
-            results.append( {"created": created, "llc": result} )
-        except Exception as e:
-            logging.error(f"Failed to update or create LLC: {str(e)}")
-            raise HTTPException(status_code=400, detail=str(e))
-    return results
 ############################################################################################################
 
 @router.delete(
@@ -178,20 +128,4 @@ async def delete_llc(llcs_data: llcsDelListDTO, db: AsyncSession = Depends(get_s
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
     return results
-############################################################################################################
-
-@router.post(
-        "/llcsxl",
-        summary="Create or edit LLCs using excel file",
-        description="Create or edit LLCs using excel file",
-        tags=["Upsert"],
-        )
-async def create_or_update_llc_excel(llc: llcDTO, db: AsyncSession = Depends(get_session)):
-    llc_repository = LLCRepository(db)
-    llc_service = llcService(llc_repository)
-    try:
-        created, result = await llc_service.create_or_update_llc(llc)
-        return {"created": created, "llc": result}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
 ############################################################################################################
