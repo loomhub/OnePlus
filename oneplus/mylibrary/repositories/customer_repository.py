09b7d6e61d62a311022af62fd2ab24727f8c1eb9
loomhub <vjnyustern@gmail.com>
2024-05-06 09:38:41 -0400

    Completed Phase 1 Development

--- oneplus/mylibrary/repositories/customer_repository.py
+++
@@ -1,7 +0,0 @@
-# from sqlalchemy.ext.asyncio import AsyncSession
-# from .myrepository import myRepository
-
-# class customerRepository(myRepository):
-#     def __init__(self, db_session: AsyncSession):
-#         super().__init__(db_session)
-
--- oneplus/mylibrary/services/customer_service.py
+++
@@ -1,7 +0,0 @@
-# from ..repositories.customer_repository import customerRepository
-# from .myservice import MyService
-
-# class customerService(MyService):
-#     def __init__(self, customer_repository: customerRepository):
-#         super().__init__(customer_repository)
-   
